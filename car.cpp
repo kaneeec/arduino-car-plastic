f3783343fb0f77f89f194297cf8508ae272b2cd6
Tomas Zeman <tomas.zeman@oracle.com>
2016-05-16 23:26:54 +0200

    IR control replaced by BT

--- car.cpp
+++ car.cpp
@@ -1,89 +1,45 @@
 #include <Arduino.h>
-#include "libraries/DCMotor/DCMotor.h"
-//#include "libraries/IRemote/IrRemote.h"
-//#include "libraries/IRemote/IrRemoteInt.h"
 #include "libraries/BTController/BTController.h"
-
-//const long IR_OK = 16712445;
-//const long IR_LEFT = 16720605;
-//const long IR_RIGHT = 16761405;
-//const long IR_UP = 16736925;
-//const long IR_DOWN = 16754775;
-//const long IR_STAR = 16728765;
-//const long IR_HASH = 16732845;
+#include "libraries/DCMotor/DCMotor.h"
 
 DCMotor engine(2, 3);
 DCMotor steering(4, 5);
-
-//IrReceiver irReceiver(6);
-//IrReading irReading;
-
 BTController bt(10, 11);
 
-//void readIr();
 void readBtController();
 
 void setup() {
     Serial.begin(9600);
-//    irReceiver.enableIRIn();
 }
 
 void loop() {
-//    readIr();
     readBtController();
     engine.run();
     steering.run();
 }
 
-//void readIr() {
-//    if (irReceiver.decode(&irReading)) {
-//        if (irReading.value == IR_STAR) {
-//            if (engine.isRunning()) {
-//                engine.stop();
-//            } else {
-//                engine.start();
-//            }
-//            if (steering.isRunning()) {
-//                steering.stop();
-//            }
-//        } else if (irReading.value == IR_LEFT) {
-//            steering.start();
-//            steering.setDirection(FORWARDS);
-//        } else if (irReading.value == IR_RIGHT) {
-//            steering.start();
-//            steering.setDirection(BACKWARDS);
-//        } else if (irReading.value == IR_OK) {
-//            steering.stop();
-//        } else if (irReading.value == IR_UP) {
-//            engine.setDirection(FORWARDS);
-//        } else if (irReading.value == IR_DOWN) {
-//            engine.setDirection(BACKWARDS);
-//        }
-//        irReceiver.resume(); // Receive the next value
-//    }
-//}
-
 void readBtController() {
-    if (bt.parseValues()) {
-        BTControllerAngles& angles = bt.getAngles();
-        if (angles.turn < -10) {
-            steering.setDirection(FORWARDS);
-            steering.start();
-        } else if (angles.turn > 10) {
-            steering.setDirection(BACKWARDS);
-            steering.start();
-        } else {
-            steering.stop();
-        }
+    if (bt.read()) {
+        String value = bt.getValue();
 
-        if (angles.forward < 20) {
+        if (value.indexOf('1') >= 0) {
             engine.setDirection(FORWARDS);
             engine.start();
-        } else if (angles.forward > 40) {
+        } else if (value.indexOf('2') >= 0) {
             engine.setDirection(BACKWARDS);
             engine.start();
         } else {
             engine.stop();
+        }
+
+        if (value.indexOf('3') >= 0) {
+            steering.setDirection(FORWARDS);
+            steering.start();
+        } else if (value.indexOf('4') >= 0) {
+            steering.setDirection(BACKWARDS);
+            steering.start();
+        } else {
+            steering.stop();
         }
     }
 }
